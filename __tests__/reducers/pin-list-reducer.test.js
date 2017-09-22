478103cf08d1f0920b5899a3f5865d7dd48abfb1
Brittany Kerr <kerrbrittany9@gmail.com>
2017-09-22 13:22:09 -0700

    write a red-green test for ADD PIN

--- __tests__/reducers/pin-list-reducer.test.js
+++ __tests__/reducers/pin-list-reducer.test.js
@@ -1,6 +1,26 @@
-import 
+import pinList from './../../src/reducers/pin-list-reducer';
+import c from './../../src/constants';
+
+
 describe("Pin list reducer", () => {
-  test('temporary dummy test: one plus one is two', () => {
-    expect(1+1).toBe(2);
+  let action;
+  const pinInfo = {
+    title: 'all my thoughts',
+    content: 'wow! darn! jeez!',
+    id: 0,
+    repin: 0
+  };
+
+  test('should add pin to array', () => {
+    const { title, content, id, repin } = pinInfo;
+    action = {
+      type: c.ADD_PIN,
+      title: title,
+      content: content,
+      id: id,
+      repin: repin
+    };
+    const futureState = [ pinInfo ];
+    expect(pinList([], action)).toEqual([ pinInfo ]);
   });
 });
